57a5ad14cc4adbc45a709ae26d0847c2b56a1268
edmond120 <edmondwong888@msn.com>
2017-11-21 23:55:57 -0500

    added notes

---
+++ smallestMultiple.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int evenDevisiable(num){
+
+}
+int main(){
+	int num = 1;
+	while(evenDivisiable(num) == 0){
+		num++;
+	}
+	printf("smallest positive number evenly divisiable by 1-20 %d\n",num);
+}
